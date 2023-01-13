c07361a6f0ebc1c0810718f0b97253d80107b459
Jaisri5987 <74231611+Jaisri5987@users.noreply.github.com>
2023-01-13 11:25:08 +0530

    Add files via upload

---
+++ DAY_2 ASSESSMENT/myjavascript.js
@@ -0,0 +1,29 @@
+function my_javascript()
+{
+   alert("added successfully");   
+}
+
+
+function save(event)
+{
+  event.preventDefault();
+    var forms=document.getElementById("addmobileform");
+    var input=forms.querySelectorAll("input,select");
+    let output={};
+
+    
+    for( var inputNode of input)
+    {
+        let value=inputNode?inputNode.value:null;
+        if(!value)
+        {
+            alert(inputNode.id+"PLEASE FILL THE FIELDS");
+            document.getElementById(inputNode.id).focus();
+            return;
+        }
+        output[inputNode.id]=inputNode.value;
+    }
+    alert("Inputs are added successfully");
+    console.log("inputs are added!!!");
+
+}
